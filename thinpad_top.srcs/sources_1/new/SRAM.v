e7e450e3db02b89a324a2aa0729d398f25e7d038
chenxz <792146066@qq.com>
2021-11-08 01:34:14 +0800

    sram without uart

--- thinpad_top.srcs/sources_1/new/SRAM.v
+++ thinpad_top.srcs/sources_1/new/SRAM.v
@@ -5,9 +5,10 @@ module SRAM(
 
     // clk_btn, rst_btn
 
-
+    input wire oe,
+    input wire we,
     input wire[3:0] be_n,
-    input wire[19:0] address,//may be need to modify
+    input wire[31:0] address,//may be need to modify
     input wire[31:0] data_in,
     output wire[31:0] data_out,
 
@@ -33,25 +34,32 @@ module SRAM(
     input wire uart_dataready,
     input wire uart_tbre,
     input wire uart_tsre,
-
-    //input sigals
-    input wire read_base,
-    input wire write_base,
-    input wire read_ext,
-    input wire write_ext,
-    input wire read_uart,
-    input wire write_uart
     );
 
+//controll sigals
+wire read_base;
+wire write_base;
+wire read_ext;
+wire write_ext;
+wire read_uart;
+wire write_uart;
+
+assign read_base = oe && (address >= 32'h80000000) && (address <= 32'h803fffff);
+assign write_base = we && (address >= 32'h80000000) && (address <= 32'h803fffff);
+assign read_ext = oe && (address >= 32'h80400000) && (address <= 32'h807fffff);
+assign write_ext = we && (address >= 32'h80400000) && (address <= 32'h807fffff);
+assign read_uart = oe && (address == 32'h10000000);
+assign write_uart = we && (address == 32'h10000000);
+
 assign base_ram_data_wire = (write_base || write_uart) ? data_in : 32'bz;
-assign base_ram_addr = address;
+assign base_ram_addr = address[19:0];
 assign base_ram_be_n = be_n;
 assign base_ram_ce_n = read_uart || write_uart;
 assign base_ram_oe_n = read_uart || write_uart;
 assign base_ram_we_n = write_base ? clk : 1'b1;
 
 assign ext_ram_data_wire = write_ext ? data_in : 32'bz;
-assign ext_ram_addr = address;
+assign ext_ram_addr = address[19:0];
 assign ext_ram_be_n = be_n;
 assign ext_ram_ce_n = 1'b0;
 assign ext_ram_oe_n = 1'b0;
