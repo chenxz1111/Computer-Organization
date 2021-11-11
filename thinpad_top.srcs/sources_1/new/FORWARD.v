c7d1c57d2ac92f4ba61a44dc5cd0375796f53145
chenxz <792146066@qq.com>
2021-11-11 15:37:03 +0800

    forward

---
+++ thinpad_top.srcs/sources_1/new/FORWARD.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/11/11 15:36:22
+// Design Name: 
+// Module Name: FORWARD
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FORWARD(
+
+    );
+endmodule
