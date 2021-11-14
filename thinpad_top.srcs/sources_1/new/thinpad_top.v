d69f4cde882f734ce021c67921b827f85e3368a6
wbt <wangbt19@mails.tsinghua.edu.cn>
2021-11-14 19:56:14 +0800

    change clk

--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -5,51 +5,51 @@ module thinpad_top(
     input wire clk_50M,           //50MHz 时钟输入
     input wire clk_11M0592,       //11.0592MHz 时钟输入（备用，可不用）
 
-    input wire clock_btn,         //BTN5手动时钟按钮�?关，带消抖电路，按下时为1
-    input wire reset_btn,         //BTN6手动复位按钮�?关，带消抖电路，按下时为1
+    input wire clock_btn,         //BTN5手动时钟按钮�??关，带消抖电路，按下时为1
+    input wire reset_btn,         //BTN6手动复位按钮�??关，带消抖电路，按下时为1
 
     input  wire[3:0]  touch_btn,  //BTN1~BTN4，按钮开关，按下时为1
-    input  wire[31:0] dip_sw,     //32位拨码开关，拨到“ON”时�?1
+    input  wire[31:0] dip_sw,     //32位拨码开关，拨到“ON”时�??1
     output wire[15:0] leds,       //16位LED，输出时1点亮
     output wire[7:0]  dpy0,       //数码管低位信号，包括小数点，输出1点亮
     output wire[7:0]  dpy1,       //数码管高位信号，包括小数点，输出1点亮
 
-    //CPLD串口控制器信�?
-    output wire uart_rdn,         //读串口信号，低有�?
-    output wire uart_wrn,         //写串口信号，低有�?
-    input wire uart_dataready,    //串口数据准备�?
-    input wire uart_tbre,         //发�?�数据标�?
-    input wire uart_tsre,         //数据发�?�完毕标�?
+    //CPLD串口控制器信�??
+    output wire uart_rdn,         //读串口信号，低有�??
+    output wire uart_wrn,         //写串口信号，低有�??
+    input wire uart_dataready,    //串口数据准备�??
+    input wire uart_tbre,         //发�?�数据标�??
+    input wire uart_tsre,         //数据发�?�完毕标�??
 
     //BaseRAM信号
-    inout wire[31:0] base_ram_data,  //BaseRAM数据，低8位与CPLD串口控制器共�?
+    inout wire[31:0] base_ram_data,  //BaseRAM数据，低8位与CPLD串口控制器共�??
     output wire[19:0] base_ram_addr, //BaseRAM地址
-    output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。如果不使用字节使能，请保持�?0
-    output wire base_ram_ce_n,       //BaseRAM片�?�，低有�?
-    output wire base_ram_oe_n,       //BaseRAM读使能，低有�?
-    output wire base_ram_we_n,       //BaseRAM写使能，低有�?
+    output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。如果不使用字节使能，请保持�??0
+    output wire base_ram_ce_n,       //BaseRAM片�?�，低有�??
+    output wire base_ram_oe_n,       //BaseRAM读使能，低有�??
+    output wire base_ram_we_n,       //BaseRAM写使能，低有�??
 
     //ExtRAM信号
     inout wire[31:0] ext_ram_data,  //ExtRAM数据
     output wire[19:0] ext_ram_addr, //ExtRAM地址
-    output wire[3:0] ext_ram_be_n,  //ExtRAM字节使能，低有效。如果不使用字节使能，请保持�?0
-    output wire ext_ram_ce_n,       //ExtRAM片�?�，低有�?
-    output wire ext_ram_oe_n,       //ExtRAM读使能，低有�?
-    output wire ext_ram_we_n,       //ExtRAM写使能，低有�?
+    output wire[3:0] ext_ram_be_n,  //ExtRAM字节使能，低有效。如果不使用字节使能，请保持�??0
+    output wire ext_ram_ce_n,       //ExtRAM片�?�，低有�??
+    output wire ext_ram_oe_n,       //ExtRAM读使能，低有�??
+    output wire ext_ram_we_n,       //ExtRAM写使能，低有�??
 
     //直连串口信号
     output wire txd,  //直连串口发�?�端
-    input  wire rxd,  //直连串口接收�?
+    input  wire rxd,  //直连串口接收�??
 
     //Flash存储器信号，参�?? JS28F640 芯片手册
-    output wire [22:0]flash_a,      //Flash地址，a0仅在8bit模式有效�?16bit模式无意�?
+    output wire [22:0]flash_a,      //Flash地址，a0仅在8bit模式有效�??16bit模式无意�??
     inout  wire [15:0]flash_d,      //Flash数据
     output wire flash_rp_n,         //Flash复位信号，低有效
-    output wire flash_vpen,         //Flash写保护信号，低电平时不能擦除、烧�?
-    output wire flash_ce_n,         //Flash片�?�信号，低有�?
-    output wire flash_oe_n,         //Flash读使能信号，低有�?
-    output wire flash_we_n,         //Flash写使能信号，低有�?
-    output wire flash_byte_n,       //Flash 8bit模式选择，低有效。在使用flash�?16位模式时请设�?1
+    output wire flash_vpen,         //Flash写保护信号，低电平时不能擦除、烧�??
+    output wire flash_ce_n,         //Flash片�?�信号，低有�??
+    output wire flash_oe_n,         //Flash读使能信号，低有�??
+    output wire flash_we_n,         //Flash写使能信号，低有�??
+    output wire flash_byte_n,       //Flash 8bit模式选择，低有效。在使用flash�??16位模式时请设�??1
 
     //USB 控制器信号，参�?? SL811 芯片手册
     output wire sl811_a0,
@@ -72,13 +72,28 @@ module thinpad_top(
     input  wire dm9k_int,
 
     //图像输出信号
-    output wire[2:0] video_red,    //红色像素�?3�?
-    output wire[2:0] video_green,  //绿色像素�?3�?
-    output wire[1:0] video_blue,   //蓝色像素�?2�?
-    output wire video_hsync,       //行同步（水平同步）信�?
-    output wire video_vsync,       //场同步（垂直同步）信�?
+    output wire[2:0] video_red,    //红色像素�??3�??
+    output wire[2:0] video_green,  //绿色像素�??3�??
+    output wire[1:0] video_blue,   //蓝色像素�??2�??
+    output wire video_hsync,       //行同步（水平同步）信�??
+    output wire video_vsync,       //场同步（垂直同步）信�??
     output wire video_clk,         //像素时钟输出
-    output wire video_de           //行数据有效信号，用于区分消隐�?
+    output wire video_de           //行数据有效信号，用于区分消隐�??
+);
+
+// PLL��Ƶʾ��
+wire locked, clk_10M, clk_20M;
+pll_example clock_gen 
+(
+ // Clock in ports
+ .clk_in1(clk_50M),  // �ⲿʱ������
+ // Clock out ports
+ .clk_out1(clk_10M), // ʱ�����1��Ƶ����IP���ý���������
+ .clk_out2(clk_20M), // ʱ�����2��Ƶ����IP���ý���������
+ // Status and control signals
+ .reset(reset_btn), // PLL��λ����
+ .locked(locked)    // PLL����ָʾ�����"1"��ʾʱ���ȶ���
+                    // �󼶵�·��λ�ź�Ӧ���������ɣ����£�
 );
 
 localparam NOP = 32'b00000000000000000000000000010011;
@@ -185,7 +200,7 @@ reg[31:0] address;
 reg[31:0] data_in;
 wire[31:0] data_out;
 SRAM _SRAM (
-    .clk            (clk_50M),
+    .clk            (clk_20M),
     .oe(oe),
     .we(we),
     .be(be),
@@ -215,7 +230,7 @@ SRAM _SRAM (
 );
 
 REG _REG(
-    .clk            (clk_50M),
+    .clk            (clk_20M),
     .rst            (reset_btn),
     .we(r4_reg_sel),
     .waddr(r4_instr[11:7]),
@@ -287,7 +302,7 @@ WBSEL _WBSEL(
     .wb_data(r3_wb_data)
 );
 
-always @(posedge clk_50M or posedge reset_btn) begin
+always @(posedge clk_20M or posedge reset_btn) begin
     if (reset_btn) begin
         debug_leds <= 16'hffff; // JUST FOR DEBUG_____#0xffff_____
         r0_pc <= 32'h80000000;
