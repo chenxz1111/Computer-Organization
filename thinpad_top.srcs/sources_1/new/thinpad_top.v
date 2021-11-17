70775946b9d7e505f876e57eda288a573610e0e7
wbt <wangbt19@mails.tsinghua.edu.cn>
2021-11-17 16:46:17 +0800

    add example codes

--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -432,4 +432,18 @@ always @(posedge clk_25M or posedge reset_btn) begin
     end
 end
 
+wire [11:0] hdata;
+assign video_red = hdata < 266 ? 3'b111 : 0; //红色竖条
+assign video_green = hdata < 532 && hdata >= 266 ? 3'b111 : 0; //绿色竖条
+assign video_blue = hdata >= 532 ? 2'b11 : 0; //蓝色竖条
+assign video_clk = clk_50M;
+vga #(12, 800, 856, 976, 1040, 600, 637, 643, 666, 1, 1) vga800x600at75 (
+   .clk(clk_50M), 
+   .hdata(hdata), //横坐标
+   .vdata(),      //纵坐标
+   .hsync(video_hsync),
+   .vsync(video_vsync),
+   .data_enable(video_de)
+);
+
 endmodule
