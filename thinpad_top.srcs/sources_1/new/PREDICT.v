59d8fd2fabd6e9c17b5b2d77d8feb47cdb5aca7d
ganqy <2019011240@secoder.net>
2021-11-17 20:52:57 +0800

    predict

--- thinpad_top.srcs/sources_1/new/PREDICT.v
+++ thinpad_top.srcs/sources_1/new/PREDICT.v
@@ -127,25 +127,25 @@ always @(posedge clk or posedge rst) begin
         handle_type <= 3'h0;
         handle_target <= 3'h0;
     end
-    // else begin
-    //     if (handle_type == `ADD_TARGET) begin //把表项移到最前面
-    //         btb_pc[0] <= r2_pc;
-    //         btb_pc[1] <= btb_pc[0];
-    //         btb_pc[2] <= btb_pc[1];
-    //         btb_pc[3] <= btb_pc[2];
-    //         btb_pc[4] <= btb_pc[3];
-    //         btb_pc[5] <= btb_pc[4];
-    //         btb_pc[6] <= btb_pc[5];
-    //         btb_pc[7] <= btb_pc[6];
-    //         btb_next[0] <= real_next_pc;
-    //         btb_next[1] <= btb_next[0];
-    //         btb_next[2] <= btb_next[1];
-    //         btb_next[3] <= btb_next[2];
-    //         btb_next[4] <= btb_next[3];
-    //         btb_next[5] <= btb_next[4];
-    //         btb_next[6] <= btb_next[5];
-    //         btb_next[7] <= btb_next[6];
-    //     end
+    else begin
+        if (handle_type == `ADD_TARGET) begin //把表项移到最前面
+            btb_pc[0] <= r2_pc;
+            btb_pc[1] <= btb_pc[0];
+            btb_pc[2] <= btb_pc[1];
+            btb_pc[3] <= btb_pc[2];
+            btb_pc[4] <= btb_pc[3];
+            btb_pc[5] <= btb_pc[4];
+            btb_pc[6] <= btb_pc[5];
+            btb_pc[7] <= btb_pc[6];
+            btb_next[0] <= real_next_pc;
+            btb_next[1] <= btb_next[0];
+            btb_next[2] <= btb_next[1];
+            btb_next[3] <= btb_next[2];
+            btb_next[4] <= btb_next[3];
+            btb_next[5] <= btb_next[4];
+            btb_next[6] <= btb_next[5];
+            btb_next[7] <= btb_next[6];
+        end
     //     else if (handle_type == `DELETE_TARGET) begin
     //         btb_pc[0] <= (handle_target > 0) ? btb_pc[0] : btb_pc[1];
     //         btb_pc[1] <= (handle_target > 1) ? btb_pc[1] : btb_pc[2];
@@ -182,7 +182,7 @@ always @(posedge clk or posedge rst) begin
     //         btb_next[6] <= (handle_target < 6) ? btb_next[6] : btb_next[5];
     //         btb_next[7] <= (handle_target < 7) ? btb_next[7] : btb_next[6];
     //     end
-    // end
+    end
 end
 
 
