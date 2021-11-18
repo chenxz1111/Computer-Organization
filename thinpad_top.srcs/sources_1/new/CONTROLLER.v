f81dd423098c87687a025b1bf4d0c76d3c709312
chenxz <792146066@qq.com>
2021-11-18 23:56:40 +0800

    csr

--- thinpad_top.srcs/sources_1/new/CONTROLLER.v
+++ thinpad_top.srcs/sources_1/new/CONTROLLER.v
@@ -163,18 +163,18 @@ always @(*) begin
             reg_sel = 1'b0;
             wb_sel = `NO_WB;
         end
-        default: begin
-            pc_sel = 1'b0;
-            imm_sel = `N_IMM;
-            data_a_sel = 1'b0;
-            data_b_sel = 1'b0;
-            data_type = 1'b0;
-            alu_sel = `ADD;
-            bq_sel = `NO_BQ;
-            mem_sel = `NO_RAM;
-            reg_sel = 1'b0;
-            wb_sel = `NO_WB;
-        end
+        // default: begin
+        //     pc_sel = 1'b0;
+        //     imm_sel = `N_IMM;
+        //     data_a_sel = 1'b0;
+        //     data_b_sel = 1'b0;
+        //     data_type = 1'b0;
+        //     alu_sel = `ADD;
+        //     bq_sel = `NO_BQ;
+        //     mem_sel = `NO_RAM;
+        //     reg_sel = 1'b0;
+        //     wb_sel = `NO_WB;
+        // end
     endcase
 end
 
