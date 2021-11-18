f63d7022b920d3390fbdab375c766c6e73672bf4
chenxz <792146066@qq.com>
2021-11-18 23:20:17 +0800

    csr

--- thinpad_top.srcs/sources_1/new/CONTROLLER.v
+++ thinpad_top.srcs/sources_1/new/CONTROLLER.v
@@ -203,18 +203,26 @@ always @(*) begin
                     wb_sel = `ALU_WB;
                 end
                 3'b000: begin // ebreak, ecall, mret
-                    
+                    pc_sel = 1'b1;
+                    imm_sel = `I_IMM;
+                    data_a_sel = 1'b0;
+                    data_b_sel = 1'b1;
+                    data_type = 1'b0;
                     case (instr[31:20])
                         12'h000: begin //ecall
-                            
+                            alu_sel = `CSRRW;
                         end 
                         12'h001: begin //ebreak
-                            
+                            alu_sel = `CSRRW;
                         end
                         12'h302: begin //mret
-                            
+                            alu_sel = `CSRRW;
                         end
                     endcase
+                    bq_sel = `JMP_BQ;
+                    mem_sel = `NO_RAM;
+                    reg_sel = 1'b1;
+                    wb_sel = `PC_WB;
                 end
             endcase
         end
