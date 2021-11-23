01a1997c9fd45eeaa9ba14bed7dad146d83e085e
ganqy <2019011240@secoder.net>
2021-11-23 20:52:34 +0800

    v2

--- thinpad_top.srcs/sources_1/new/TLB.v
+++ thinpad_top.srcs/sources_1/new/TLB.v
@@ -35,12 +35,6 @@ localparam
     lw = 2,
     sb = 3,
     sw = 4;
-localparam
-    check = 3'b000,
-    wait_VPN1 = 3'b001,
-    check_VPN1 = 3'b010,
-    wait_VPN2 = 3'b011,
-    check_VPN2 = 3'b100;
 
 reg[31:0] r2_instr_reg;
 reg[31:0] b_reg;
@@ -48,103 +42,19 @@ reg done;
 wire conflict_reg;
 assign conflict_reg = (command != fetch);
 wire[6:0] opcode;
-assign opcode = (state == check)? r2_instr[6:0]: r2_instr_reg[6:0];
+assign opcode = (epoch == `ALL_TARGET)? r2_instr[6:0]: r2_instr_reg[6:0];
 wire[2:0] funct3;
-assign funct3 = (state == check)? r2_instr[14:12]: r2_instr_reg[14:12];
-
+assign funct3 = (epoch == `ALL_TARGET)? r2_instr[14:12]: r2_instr_reg[14:12];
 reg[2:0] command;
-always @(*) begin
-    if(done) begin
-        command = fetch;
-    end
-    else begin
-        case(opcode)
-            load_code: begin
-                case(funct3)
-                    funct_LB: command = lb;
-                    funct_LW: command = lw;
-                    default: command = fetch;
-                endcase
-            end
-            store_code: begin
-                case(funct3)
-                    funct_SB: command = sb;
-                    funct_SW: command = sw;
-                    default: command = fetch;
-                endcase
-            end
-            default: begin
-                command = fetch;
-            end
-        endcase
-    end
-end
 reg[9:0] VPN1_reg;
 reg[31:0] PTE1_reg;
 reg[9:0] VPN2_reg;
 reg[31:0] PTE2_reg;
-reg[2:0] state;
+reg[2:0] epoch;
 wire[31:0] addr_src;
-assign addr_src = (command == fetch) ? ( mem_stall ? r0_pc : error ? next_pc : predict_pc) : r2_alu_res;
+assign addr_src = !conflict_reg ? ( mem_stall ? r0_pc : error ? next_pc : predict_pc) : r2_alu_res;
 reg[31:0] addr_src_reg;
-
-always@* begin
-    case(state)
-        check: begin
-            if(csr_status == 1'b0) begin
-                if(addr_src[31:22] == VPN1_reg) begin
-                    if(addr_src[21:12] == VPN2_reg) begin//直接计算
-                        r3_stall = (command != fetch);
-                        r3_ram_enable = 1'b1;
-                        r3_addr = {PTE2_reg[29:10], addr_src[11:0]};
-                    end
-                    else begin//已匹配VPN1
-                        r3_stall = 1'b1;
-                        r3_ram_enable = 1'b0;
-                        r3_addr = {PTE1_reg[29:10], addr_src[21:12], 2'b00};
-                    end
-                end
-                else begin
-                    r3_stall = 1'b1;
-                    r3_ram_enable = 1'b0;
-                    r3_addr = {csr_satp[19:0], addr_src[31:22], 2'b00};
-                end
-            end
-            else begin
-                r3_stall = (command != fetch);
-                r3_ram_enable = 1'b1;
-                r3_addr = addr_src;
-            end
-        end
-        wait_VPN1: begin
-            r3_stall = 1'b1;
-            r3_ram_enable = 1'b0;
-            r3_addr = addr_src_reg;
-        end
-        check_VPN1: begin
-            r3_stall = 1'b1;
-            r3_ram_enable = 1'b0;
-            r3_addr = {sram_data_out[29:10], addr_src_reg[21:12], 2'b00};
-        end
-        wait_VPN2: begin
-            r3_stall = 1'b1;
-            r3_ram_enable = 1'b0;
-            r3_addr = addr_src_reg;
-        end
-        check_VPN2: begin
-            r3_stall = (command != fetch);
-            r3_ram_enable = 1'b1;
-            r3_addr = {sram_data_out[29:10], addr_src_reg[11:0]};
-        end
-        default: begin
-            r3_stall = 1'b1;
-            r3_ram_enable = 1'b0;
-            r3_addr = addr_src;
-        end
-    endcase
-end
-
-assign r3_data_in = (state == check)? forward_data_b: b_reg;
+assign r3_data_in = (epoch == `ALL_TARGET)? forward_data_b: b_reg;
 
 always @(*) begin
     case(command)
@@ -175,21 +85,104 @@ always @(*) begin
         end
     endcase
 end
+always @(*) begin
+    if(done) begin
+        command = fetch;
+    end
+    else begin
+        case(opcode)
+            load_code: begin
+                case(funct3)
+                    funct_LB: command = lb;
+                    funct_LW: command = lw;
+                    default: command = fetch;
+                endcase
+            end
+            store_code: begin
+                case(funct3)
+                    funct_SB: command = sb;
+                    funct_SW: command = sw;
+                    default: command = fetch;
+                endcase
+            end
+            default: begin
+                command = fetch;
+            end
+        endcase
+    end
+end
+
+
+always@* begin
+    case(epoch)
+        `ALL_TARGET: begin
+            if(csr_status == 1'b0) begin
+                if(addr_src[31:22] == VPN1_reg) begin
+                    if(addr_src[21:12] == VPN2_reg) begin//直接计算
+                        r3_stall = conflict_reg;
+                        r3_ram_enable = 1'b1;
+                        r3_addr = {PTE2_reg[29:10], addr_src[11:0]};
+                    end
+                    else begin//已匹配VPN1
+                        r3_stall = 1'b1;
+                        r3_ram_enable = 1'b0;
+                        r3_addr = {PTE1_reg[29:10], addr_src[21:12], 2'b00};
+                    end
+                end
+                else begin
+                    r3_stall = 1'b1;
+                    r3_ram_enable = 1'b0;
+                    r3_addr = {csr_satp[19:0], addr_src[31:22], 2'b00};
+                end
+            end
+            else begin
+                r3_stall = conflict_reg;
+                r3_ram_enable = 1'b1;
+                r3_addr = addr_src;
+            end
+        end
+        `GET_FIRST: begin
+            r3_stall = 1'b1;
+            r3_ram_enable = 1'b0;
+            r3_addr = addr_src_reg;
+        end
+        `FIRST_TARGET: begin
+            r3_stall = 1'b1;
+            r3_ram_enable = 1'b0;
+            r3_addr = {sram_data_out[29:10], addr_src_reg[21:12], 2'b00};
+        end
+        `GET_SECOND: begin
+            r3_stall = 1'b1;
+            r3_ram_enable = 1'b0;
+            r3_addr = addr_src_reg;
+        end
+        `SECOND_TARGET: begin
+            r3_stall = conflict_reg;
+            r3_ram_enable = 1'b1;
+            r3_addr = {sram_data_out[29:10], addr_src_reg[11:0]};
+        end
+        default: begin
+            r3_stall = 1'b1;
+            r3_ram_enable = 1'b0;
+            r3_addr = addr_src;
+        end
+    endcase
+end
 
 always@(posedge clk or posedge rst) begin
     if(rst) begin
         done <= 1'b0;
         VPN1_reg <= 10'h3ff;
         VPN2_reg <= 10'h3ff;
-        state <= check;
+        epoch <= `ALL_TARGET;
     end
     else begin
-        case(state)
-            check: begin
+        case(epoch)
+            `ALL_TARGET: begin
                 if(csr_status == 1'b0) begin
                     if(addr_src[31:22] == VPN1_reg) begin
                         if(addr_src[21:12] == VPN2_reg) begin
-                            state <= check;//直接计算
+                            epoch <= `ALL_TARGET;//直接计算
                             if(done) begin
                                 done <= 1'b0;
                             end
@@ -199,7 +192,7 @@ always@(posedge clk or posedge rst) begin
                         end
                         else begin
                             VPN2_reg <= addr_src[21:12];
-                            state <= wait_VPN2;//已匹配VPN1
+                            epoch <= `GET_SECOND;//已匹配VPN1
                             addr_src_reg <= addr_src;
                             r2_instr_reg <= r2_instr;
                             b_reg <= forward_data_b;
@@ -208,14 +201,14 @@ always@(posedge clk or posedge rst) begin
                     else begin
                         VPN1_reg <= addr_src[31:22];
                         VPN2_reg <= addr_src[21:12];
-                        state <= wait_VPN1;
+                        epoch <= `GET_FIRST;
                         addr_src_reg <= addr_src;
                         r2_instr_reg <= r2_instr;
                         b_reg <= forward_data_b;
                     end
                 end
                 else begin
-                    state <= check;//直接计算
+                    epoch <= `ALL_TARGET;//直接计算
                     if(done) begin
                         done <= 1'b0;
                     end
@@ -224,15 +217,15 @@ always@(posedge clk or posedge rst) begin
                     end
                 end
             end
-            wait_VPN1: state <= check_VPN1;
-            check_VPN1: begin
+            `GET_FIRST: epoch <= `FIRST_TARGET;
+            `FIRST_TARGET: begin
                 PTE1_reg <= sram_data_out;
-                state <= wait_VPN2;
+                epoch <= `GET_SECOND;
             end
-            wait_VPN2: state <= check_VPN2;
-            check_VPN2: begin
+            `GET_SECOND: epoch <= `SECOND_TARGET;
+            `SECOND_TARGET: begin
                 PTE2_reg <= sram_data_out; //把sram的输出作为实地址？？？？
-                state <= check;
+                epoch <= `ALL_TARGET;
                 if(done) begin
                     done <= 1'b0;
                 end
@@ -240,7 +233,7 @@ always@(posedge clk or posedge rst) begin
                     done <= 1'b1;
                 end
             end
-            default: state <= check;
+            default: epoch <= `ALL_TARGET;
         endcase
     end
 end
